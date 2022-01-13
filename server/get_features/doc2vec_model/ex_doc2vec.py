175434f3412e0cb2c4fb1d7f7a637b1628ce2ef4
c-chocolate <97207833+c-chocolate@users.noreply.github.com>
2022-01-13 15:48:18 +0900

    Add files via upload

--- server/get_features/doc2vec_model/ex_doc2vec.py
+++ server/get_features/doc2vec_model/ex_doc2vec.py
@@ -12,6 +12,8 @@ import requests
 import re
 import copy
 import csv
+import pandas as pd
+import numpy as np
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'}
 
@@ -96,8 +98,9 @@ def decode_and_split(js):
     jssplit_result = []
     try:
         decodejs = js.decode('utf-8')
-        jssplit = decodejs.split('\n')
-        
+        #jssplit = decodejs.split('\n')
+        jssplit = re.split('[\n,\r,\r\n]', decodejs)
+
         for a in jssplit:
             if a !='':
                 a = a.replace(' ','')
@@ -107,79 +110,102 @@ def decode_and_split(js):
 
     return jssplit_result
 
-mode = 0 #0:test 1:exnew 2:ex途中       !!
+mode = 3 #0:test 1:black 2:white 3:model       !!
 
-if mode == 0:
+if mode == 0 :
     f1 = open('test.txt','r',encoding='utf-8', errors="", newline="") 
 elif mode == 1:
     f1 = open('blacklist.txt','r',encoding='utf-8', errors="", newline="")
+elif mode ==2:
     f2 = open('tranco_100k.txt','r',encoding='utf-8', errors="", newline="")
 
 #blacklist:16637行
 
 #domain_num = 0
 
-blacklist = f1.readlines()
-if mode ==1:
+if mode==0 or mode==1:
+    blacklist = f1.readlines()
+elif mode ==2:
     whitelist = f2.readlines()
 
-f3 = open('jsdatalist.txt', 'w')
-f4 = open('jsdata_domainnum.txt', 'w')
+#f3 = open('jsdatalist.csv', 'w')
+#f4 = open('jsdata_domainnum.txt', 'w')
 #for x in list_row:
 #    f.write(str(x) + "\n")
 #f.close()
 
 jsdata=[]
 datanum=0
-
+modetwo_datanum=-1
+"""
 if mode==2:
-    f5 = open("./jsdatalist.txt","r")
-    for x in f5:
-        jsdata.append(x.rstrip("\n"))
+    f5 = open("./testcsv.csv","r")
+    reader=csv.reader(f5)
+    jsdata=[e for e in reader]
+    #for x in f5:
+    #    jsdata.append(x)
         #以下のようにしてしまうと、改行コードがlistに入ってしまうため注意
         #list_row.append(x)
+    #jsdata = pd.read_csv("jsdatalist.csv").values.tolist()
     f5.close()
-    modetwo_datanum=0       # !!jsdata_domainnum.txtの数入れる
+    modetwo_datanum=0       # !!jsdata_domainnum.txtの数-1入れる
 
-    
+    #print(jsdata[1][0])
+ """   
 
 
 index = 0
 
-for data in blacklist: 
-    if not data == '' and datanum>modetwo_datanum:
-        data = data.rstrip('\n')
-        domain = data.rstrip('\r')
-        datanum+=1
-        print("blacklist",datanum,domain)
-        if get_html(domain):
-            #domain_num += 1
-            jslink, jsurllink = get_js_link(domain)
-            print(jslink,jsurllink)
-            if jslink!=None:
-                for index,value in enumerate(jslink):
-                    js = link_to_code(domain, value)
-                    if js:
-                        jssplit = decode_and_split(js)
-                        jsdata.append(jssplit)
-                        f3.write(str(jssplit) + "\n")
+if mode==0 or mode==1:
+    for data in blacklist: 
+        if not data == '' and datanum>modetwo_datanum:
+            data = data.rstrip('\n')
+            domain = data.rstrip('\r')
+            datanum+=1
+            print("<<blacklist>>",datanum,domain)
+            if get_html(domain):
+                #domain_num += 1
+                jslink, jsurllink = get_js_link(domain)
+                print(jslink,jsurllink)
+                if jslink!=None:
+                    for index,value in enumerate(jslink):
+                        js = link_to_code(domain, value)
+                        if js:
+                            jssplit = decode_and_split(js)
+                            jsdata.append(jssplit)
+                            np_jsdata= np.array(jsdata)
+                            np.save('np_black_jsdata',np_jsdata)
+                            #print(jssplit[0])
+                            #f3.write(str(jssplit) + "\n")
 
-            if jsurllink!=None:         
-                for index,value in enumerate(jsurllink):
-                    js = url_to_code(value)
-                    if js:
-                        jssplit = decode_and_split(js)
-                        jsdata.append(jssplit)
-                        f3.write(str(jssplit) + "\n")
-        f4.write(str(domainnum)+str(domain)+"\n")
-    elif datanum=<modetwo_datanum:
-        datanum+=1
+                if jsurllink!=None:         
+                    for index,value in enumerate(jsurllink):
+                        js = url_to_code(value)
+                        if js:
+                            jssplit = decode_and_split(js)
+                            jsdata.append(jssplit)
+                            np_jsdata= np.array(jsdata)
+                            np.save('np_black_jsdata',np_jsdata)
+                            #f3.write(str(jssplit) + "\n")
+            f4 = open('jsdata_domainnum.txt', 'w')
+            f4.write(str(datanum)+str(domain)+"\n")
+            f4.close()
+        elif datanum<=modetwo_datanum:
+            datanum+=1
 
+#print(jsdata[0][0])
 
 print(datanum)
-domainmax=datanum   #domainmax:blacklistで読み込んだ数
+#domainmax=datanum   #domainmax:blacklistで読み込んだ数
 
-if mode ==1:
+#redata=np.load('np_jsdata.npy', allow_pickle=True)
+#reredata=redata.tolist()
+#print(reredata[0][0])
+#print("QQQ",jsdata==reredata)
+
+
+if mode ==2 :
+    domainmax=16000     # !!
     index = 0   #whitelistの現在の数
     for data in whitelist: 
         if index >= domainmax:
@@ -188,8 +214,8 @@ if mode ==1:
             index +=1
             data = data.rstrip('\n')
             domain = data.rstrip('\r')
-            datanum+=1
-            print("blacklist",datamax,"whitelist",index,domain)
+            #datanum+=1
+            print("<<whitelist>>",index,domain)
             if get_html(domain):
                 jslink, jsurllink = get_js_link(domain)
                 print(jslink,jsurllink)
@@ -199,7 +225,9 @@ if mode ==1:
                         if js:
                             jssplit = decode_and_split(js)
                             jsdata.append(jssplit)
-                            f3.write(str(jssplit) + "\n")
+                            np_jsdata= np.array(jsdata)
+                            np.save('np_white_jsdata',np_jsdata)
+                            #f3.write(str(jssplit) + "\n")
 
                 if jsurllink!=None:         
                     for index,value in enumerate(jsurllink):
@@ -207,27 +235,42 @@ if mode ==1:
                         if js:
                             jssplit = decode_and_split(js)
                             jsdata.append(jssplit)
-                            f3.write(str(jssplit) + "\n")
-            f4.write(str(domainnum)+str(domain)+"\n")
-        elif datanum=<modetwo_datanum:
+                            np_jsdata= np.array(jsdata)
+                            np.save('np_white_jsdata',np_jsdata)
+                            #f3.write(str(jssplit) + "\n")
+            f4 = open('jsdata_domainnum.txt', 'w')
+            f4.write(str(index)+str(domain)+"\n")
+            f4.close()
+        elif datanum<=modetwo_datanum:
             datanum+=1
 
-f3.close()
-f4.close()
+#f3.close()
+#f4.close()
 
-trainings = [TaggedDocument(data, [i]) for i,data in enumerate(jsdata)]
-
-# トレーニング
-m = Doc2Vec(documents= trainings, vector_size=1, dm = 0, alpha=0.003, window=16, min_count=5, sample=0.001)
-# iter=200
-
-# モデルのセーブ
-if mode == 0:
-    m.save("test.model")
-elif mode ==1:
-    m.save("doc2vec.model")
+if mode==3:
+    get_black_data=np.load('np_black_jsdata.npy', allow_pickle=True)
+    get_white_data=np.load('np_white_jsdata.npy', allow_pickle=True)
+    li_black_data=get_black_data.tolist()
+    li_white_data=get_white_data.tolist()
+    jsdata=li_black_data+li_white_data
+#print(reredata[0][0])
+#print("QQQ",jsdata==reredata)
 
 
-f1.close()
-if mode == 1:
+if mode==0 or mode==3:
+    trainings = [TaggedDocument(data, [i]) for i,data in enumerate(jsdata)]
+
+    # トレーニング
+    m = Doc2Vec(documents= trainings, vector_size=1, dm = 0, alpha=0.003, window=16, min_count=5, sample=0.001)
+    # iter=200
+
+    # モデルのセーブ
+    if mode == 0:
+        m.save("test.model")
+    elif mode ==3:
+        m.save("doc2vec.model")
+
+if mode==0 or mode==1:
+    f1.close()
+elif mode == 2:
     f2.close()
