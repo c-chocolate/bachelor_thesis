14f87bf8f6628239f51e9cec163f04de4f872087
c-chocolate <97207833+c-chocolate@users.noreply.github.com>
2022-01-17 14:45:55 +0900

    Delete for_csv directory

--- for_csv/features/__init__.py
+++
@@ -1,4 +0,0 @@
-from .contextual_features import contextual_features
-from .dns_features import dns_features
-from .domain_name_features import domain_name_features
-from .extra_features import extra_features
--- for_csv/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-base_dir="./"
-python="./venv/bin/python"
-today=$(date +%m%d)
-
-cd "$base_dir"
-"$python" "$base_dir"/generate.py "$today" 2>&1 > "$today".log
