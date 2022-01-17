3b3c7565ddebad9fc2e91434fea3c76ff299b8b7
c-chocolate <97207833+c-chocolate@users.noreply.github.com>
2022-01-17 17:21:39 +0900

    Add files via upload

---
+++ src/features/extra_features.py
@@ -0,0 +1,29 @@
+import json
+import math
+import os.path
+
+import tldextract as tld
+
+
+packagedir = os.path.dirname(__file__)
+ngram_table = None
+with open(os.path.join(packagedir, 'ngram_table.json')) as f:
+    ngram_table = json.loads(f.readline())
+
+
+def ngram(word, n):
+    return [word[i:i+n] for i in range(len(word)-n+1)]
+
+
+def extra_features(domain):
+    ext = tld.extract(domain)    
+
+    rep = 0.0
+    for s in [ext.subdomain, ext.domain]:
+        for i in range(3,8):
+            grams = ngram(s, i)
+            for gram in grams:
+                if gram in ngram_table:
+                    rep += math.log2(ngram_table[gram] / i)
+
+    return [rep]
